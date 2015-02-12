fc6bb673b0c38fa56edef8b506ccac4916ae1b9f
Liccy <the-creature@users.noreply.github.com>
2015-02-12 12:37:44 -0500

    changing css path.

--- dist/pu-video.js
+++ dist/pu-video.js
@@ -191,7 +191,7 @@ function initJQuery() {
                     
                     link.type = 'text/css'
                     link.rel = 'stylesheet'
-                    link.href = 'http://pushare.s3.amazonaws.com/pu-video/latest/app.css'
+                    link.href = 'http://pushare.s3.amazonaws.com/pu-video/latest/pu-video.css'
                     head.appendChild(link);
 
                     //load the actuall video.
